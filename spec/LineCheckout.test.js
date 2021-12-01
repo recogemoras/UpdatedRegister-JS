10b8e0dad8d3b5e2db874dc7b0b5ee707857fda1
MJ Rodríguez <recogemoras@gmail.com>
2021-12-01 10:02:35 +0000

    removed forgotten logs on testing file

--- spec/LineCheckout.test.js
+++ spec/LineCheckout.test.js
@@ -14,7 +14,7 @@ describe('Checking out line items', () => {
         for (let item in lineItems) {
             line = checkout.scan(lineItems[item]);
         }
-        //console.log(line.lineItems.get('TOTAL_PRICE'));
+
         expect(line.lineItems.get('TOTAL_PRICE')).to.equal(0);
     });
 
@@ -24,11 +24,9 @@ describe('Checking out line items', () => {
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
-        //console.log(line.lineItems.getTotalPrice());
         expect(line.lineItems.get('TOTAL_PRICE')).to.equal(32.5);
     });
 
@@ -39,7 +37,6 @@ describe('Checking out line items', () => {
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
@@ -48,12 +45,10 @@ describe('Checking out line items', () => {
 
     it ('Should show a total of 25 with one TSHIRT and two VOUCHERs in any order', () => {
         var line = new Line();
-        //console.log(line.lineItems);
         const lineItems = ['VOUCHER', 'TSHIRT', 'VOUCHER'];
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
@@ -62,12 +57,10 @@ describe('Checking out line items', () => {
 
     it ('Should show a total of 81 with 4 TSHIRTs and one VOUCHER in any order', () => {
         var line = new Line();
-        //console.log(line.lineItems);
         const lineItems = ['TSHIRT', 'TSHIRT', 'TSHIRT', 'VOUCHER', 'TSHIRT'];
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
@@ -76,12 +69,10 @@ describe('Checking out line items', () => {
 
     it ('Should show a total of 74.5 with 3 TSHIRTs, 3 VOUCHERs and one PANT in any order', () => {
         var line = new Line();
-        //console.log(line.lineItems);
         const lineItems = ['VOUCHER', 'TSHIRT', 'VOUCHER', 'VOUCHER', 'PANTS', 'TSHIRT', 'TSHIRT'];
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
@@ -90,12 +81,10 @@ describe('Checking out line items', () => {
 
     it ('Should show a total of 74.5 with 3 TSHIRTs, 4 VOUCHERs and one PANT in any order', () => {
         var line = new Line();
-        //console.log(line.lineItems);
         const lineItems = ['VOUCHER', 'TSHIRT', 'VOUCHER', 'VOUCHER', 'PANTS', 'TSHIRT', 'TSHIRT', 'VOUCHER'];
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
@@ -104,12 +93,10 @@ describe('Checking out line items', () => {
 
     it ('Should show a total of 107.5 with 5 TSHIRTs, 2 VOUCHERs and one PANT in any order', () => {
         var line = new Line();
-        //console.log(line.lineItems);
         const lineItems = ['VOUCHER', 'TSHIRT', 'TSHIRT', 'VOUCHER', 'PANTS', 'TSHIRT', 'TSHIRT', 'TSHIRT'];
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
@@ -118,12 +105,10 @@ describe('Checking out line items', () => {
 
     it ('Should show a total of 37.5 with one TSHIRT, 3 VOUCHERs, one PANT and an unidentified item, in any order', () => {
         var line = new Line();
-        //console.log(line.lineItems);
         const lineItems = ['VOUCHER', 'camisole', 'VOUCHER', 'PANTS', 'TSHIRT', 'VOUCHER'];
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
@@ -132,12 +117,10 @@ describe('Checking out line items', () => {
         
     it ('Should show a total of 45 with one TSHIRT, 3 VOUCHERs and two PANTs, in any order', () => {
         var line = new Line();
-        //console.log(line.lineItems);
         const lineItems = ['VOUCHER', 'PANTS', 'VOUCHER', 'PANTS', 'TSHIRT', 'VOUCHER'];
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
@@ -146,12 +129,10 @@ describe('Checking out line items', () => {
 
     it ('Should show a total of 65 with 2 TSHIRTs, 4 VOUCHERs and two PANTs, in any order', () => {
         var line = new Line();
-        //console.log(line.lineItems);
         const lineItems = ['VOUCHER', 'PANTS', 'VOUCHER', 'PANTS', 'TSHIRT', 'VOUCHER', 'VOUCHER', 'TSHIRT'];
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
@@ -160,12 +141,10 @@ describe('Checking out line items', () => {
 
     it ('Should show a total of 82 with 3 TSHIRTs, 4 VOUCHERs and two PANTs, in any order', () => {
         var line = new Line();
-        //console.log(line.lineItems);
         const lineItems = ['VOUCHER', 'PANTS', 'VOUCHER', 'PANTS', 'TSHIRT', 'VOUCHER', 'VOUCHER', 'TSHIRT', 'TSHIRT'];
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
@@ -174,12 +153,10 @@ describe('Checking out line items', () => {
 
     it ('Should show a total of 47.5 with one TSHIRT, one VOUCHER and 3 PANTs, in any order', () => {
         var line = new Line();
-        //console.log(line.lineItems);
         const lineItems = ['VOUCHER', 'PANTS', 'PANTS', 'TSHIRT', 'PANTS'];
         const checkout = new Checkout(pricing_rules);
 
         for (let item in lineItems) {
-            //console.log(lineItems[item]);
             line = checkout.scan(lineItems[item]);
         }
 
